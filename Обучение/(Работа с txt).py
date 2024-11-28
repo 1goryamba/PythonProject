c214f75ea517f361de98248335a35eda5b626824
1goryamba <1goryamba000@gmail.com>
2024-11-28 09:41:12 +0500

    Перенос

--- Обучение/(Работа с txt).py
+++ Обучение/(Работа с txt).py
@@ -13,3 +13,26 @@ except Exception as ex:
 
 with open("testing.txt", "w") as myfile:
     print("Работа с файлом myfile")
+
+with open("testing.txt", "w") as file:
+    file.write("hello world")
+
+print("Файл записан")
+
+with open("testing.txt", "a") as file:
+    file.write("\nhello work")
+
+print("Файл изменен")
+
+lines = ["Hello Word\n", "Hello Work\n", "Hello World\n"]
+with open("testing.txt", "a") as file:
+    file.writelines(lines)
+
+print("Список строк записан в файл")
+
+
+with open("hello.txt", "a") as myfile:
+    print("\nhello metanit.com", file=myfile)
+
+with open("testing.txt", "a") as myfile:
+    print("\nhello testing.com", file=myfile)
